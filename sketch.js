2562a59cb70522067723fccee23410a5f30fa721
Lucas Henrique <142330697+lucasornaghui@users.noreply.github.com>
2023-09-05 12:07:48 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(900, 900);
+    background('white');
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+  
+  
+  
+  
+   if(mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 20);
+   }
+  }
